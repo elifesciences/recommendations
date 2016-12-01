b16cbd9b77c5816deca198eee709d40e048a5c69
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-12-01 11:36:07 +0000

    ELPP-1868 Finished recommendations model

--- src/Recommendations/RecommendationsResponse.php
+++ src/Recommendations/RecommendationsResponse.php
@@ -2,6 +2,20 @@
 
 namespace eLife\Recommendations;
 
-class RecommendationsResponse
+use JMS\Serializer\Annotation\Since;
+use JMS\Serializer\Annotation\Type;
+
+final class RecommendationsResponse
 {
+    /**
+     * @Type("array<eLife\Recommendations\Result>")
+     * @Since(version="1")
+     */
+    public $items = [];
+
+    /**
+     * @Type("integer");
+     * @Since(version="1")
+     */
+    public $total;
 }
