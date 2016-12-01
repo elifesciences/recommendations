e6c0d9eec7c5f7b09cfd27b1a3fc35214e3f5e4e
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-12-01 11:13:37 +0000

    ELPP-1868 Added response models

--- src/Recommendations/Response/VorArticle.php
+++ src/Recommendations/Response/VorArticle.php
@@ -2,6 +2,17 @@
 
 namespace eLife\Recommendations\Response;
 
+use eLife\Api\Response\ArticleResponse\Article as ArticleFields;
+use JMS\Serializer\Annotation\Since;
+use JMS\Serializer\Annotation\Type;
+
 final class VorArticle implements Article, Response
 {
+    use ArticleFields;
+
+    /**
+     * @Type("string")
+     * @Since(version="1")
+     */
+    public $status = 'vor';
 }
