33c0b8fdd67b9477ca3c8fb14ff9244ffff19fbd
Stephen Fraser <stephen.fraser@digirati.co.uk>
2017-01-10 16:51:42 +0000

    Removed local fix

--- src/Api/Command/PopulateCommand.php
+++ src/Api/Command/PopulateCommand.php
@@ -131,7 +131,7 @@ abstract class PopulateCommand extends Command
         if ($this->showProgressBar) {
             $progress = new ProgressBar($this->output, $count);
         }
-        foreach ($items->slice(0, 100) as $item) {
+        foreach ($items as $item) {
             if ($progress) {
                 $progress->advance();
             }
