27bafbb120740b8422ac256175ab36885b5f0a9e
Stephen Fraser <stephen.fraser@digirati.co.uk>
2017-01-19 15:44:19 +0000

    Start of queue command

--- src/Recommendations/Command/QueueCommand.php
+++ src/Recommendations/Command/QueueCommand.php
@@ -7,12 +7,34 @@
 
 namespace eLife\Recommendations\Command;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class QueueCommand extends Command
 {
-    public function __construct($name = null)
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
     {
-        parent::__construct($name);
+        $this->logger = $logger;
+
+        parent::__construct(null);
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('queue:watch')
+            ->setDescription('Watches SQS for changes to articles, ')
+            ->addOption('drop', 'd', InputOption::VALUE_NONE)
+        ;
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->logger->info('this is awesome');
     }
 }
