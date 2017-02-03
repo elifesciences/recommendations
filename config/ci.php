54bf9529802fafa8efbe57c1d235241b17f7c370
Giorgio Sironi <g.sironi@elifesciences.org>
2017-02-03 15:58:50 +0000

    Added configuration for GoAWS

--- config/ci.php
+++ config/ci.php
@@ -3,5 +3,11 @@
 return [
     'debug' => false,
     'validate' => true,
-    'ttl' => 0
+    'ttl' => 0,
+    'aws' => [
+        'queue_name' => 'recommendations--ci',
+        'credential_file' => true,
+        'region' => 'us-east-1',
+        'endpoint' => 'http://localhost:4100',
+    ],
 ];
--- config/dev.php
+++ config/dev.php
@@ -3,5 +3,11 @@
 return [
     'debug' => true,
     'validate' => true,
-    'ttl' => 0
+    'ttl' => 0,
+    'aws' => [
+        'queue_name' => 'recommendations--dev',
+        'credential_file' => true,
+        'region' => 'us-east-1',
+        'endpoint' => 'http://localhost:4100',
+    ],
 ];
