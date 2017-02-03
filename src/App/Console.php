62fc245386400049d30ee8e0f113b8e878beb559
Giorgio Sironi <g.sironi@elifesciences.org>
2017-02-03 16:02:47 +0000

    With either GoAWS or real AWS, we should always access some SQS instance

--- src/App/Console.php
+++ src/App/Console.php
@@ -35,12 +35,8 @@ final class Console
         $this->console->add($app->get('console.generate_database'));
         // Set up logger.
         $this->logger = $app->get('logger');
-        try {
-            $this->console->add($app->get('console.populate_rules'));
-            $this->console->add($app->get('console.queue'));
-        } catch (SqsException $e) {
-            $this->logger->debug('Cannot connect to SQS so some commands are not available', ['exception' => $e]);
-        }
+        $this->console->add($app->get('console.populate_rules'));
+        $this->console->add($app->get('console.queue'));
     }
 
     public function queueCreateCommand()
